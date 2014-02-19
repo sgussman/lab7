7a86c0fae807fc7982e295cddaf09dcdb734f5ed
vagrant <vagrant@precise32.(none)>
2014-02-19 07:11:11 +0000

    Here's my lab 7

--- routes/project.js
+++ routes/project.js
@@ -2,9 +2,9 @@ var models = require('../models');
 
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
-
-  // query for the specific project and
-  // call the following callback
+  models.Project
+    .find({"_id": projectID})
+    .exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -15,6 +15,19 @@ exports.projectInfo = function(req, res) { 
 exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
+  console.log(form_data.project_title);
+  var newProject = new models.Project({
+      "title":form_data.project_title,
+      "date": new Date(form_data.date),
+      "summary": form_data.summary,
+      "image": form_data.image_url
+  });
+  newProject.save(afterSave);
+
+  function afterSave(err){
+    if(err){console.log(err); res.send(500);}
+    res.redirect('/');
+  }
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
@@ -22,7 +35,15 @@ exports.addProject = function(req, res) {
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
+  models.Project
+    .find({"_id":projectID})
+    .remove()
+    .exec(deleteCallback);
 
-  // find the project and remove it
+    function deleteCallback(err) {
+      if (err){console.log(err); res.send(500);}
+      res.redirect('/');
+    }
+ 
   // YOU MUST send an OK response w/ res.send();
 }
